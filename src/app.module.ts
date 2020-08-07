c48eb941f9278633ce9dc498c6d7d6157b2794af
HassanKhan123 <hk2602809@gmail.com>
2020-08-07 09:07:18 +0500

    database connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose'
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
+import {ItemsModule} from './items/items.module'
+import config from './config/keys'
 
 @Module({
-  imports: [],
+  imports: [ItemsModule,MongooseModule.forRoot(config.mongoURI)],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
